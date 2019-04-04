0aeb0dff259d1572986df5433f904cd360b599d5
Stanislav Tomilov <zeezdev@gmail.com>
2019-04-04 06:55:36 +0500

    Change api description

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_restplus import Api
 
 app = Flask(__name__)
 api = Api(version='1.0', title='Currencies API',
-          description='A simple demonstration of a Flask RestPlus powered API')
+          description='Access to rates of currencies (current and historical) by means of Rest API.')
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api.init_app(blueprint)
 app.register_blueprint(blueprint)
